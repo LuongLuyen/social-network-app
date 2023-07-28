0fae9fcab278212f92cf079d062fef734284ce84
LuongLuyen <luongvanluyen2003@gmail.com>
2023-07-28 11:41:58 +0700

    Screen Login

---
+++ src/Screens/Home.js
@@ -0,0 +1,18 @@
+import { StyleSheet,View , Text, Image} from 'react-native'
+function Home() {
+    return ( 
+        <View style={styles.container}>
+            <Text>
+                Home
+            </Text>
+        </View>
+     )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }})
+export default Home
---
+++ src/Screens/Login.js
@@ -0,0 +1,84 @@
+import {SafeAreaView, StyleSheet, TextInput,Button,View , Text} from 'react-native'
+import { useState } from 'react'
+function Login({navigation}) {
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    return ( 
+        <SafeAreaView>
+            <View style={styles.login}>
+            <View style={styles.title}>
+                <Text style={styles.title}>Social network</Text>
+            </View>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setName}
+                    placeholder="username"
+                    value={name}
+                />
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setPassword}
+                    value={password}
+                    placeholder="password"
+                />
+                <View style={styles.information}>
+                <Text>Tài khoản phải từ 6 đến 20 kí tự </Text>
+                <Text>Mật khẩu phải từ 6 đến 20 kí tự</Text>
+                </View>
+                <View style={styles.submit}>
+                <View style={styles.submitLogin}>
+                    <Button
+                        title="Login"
+                        onPress={()=> 
+                        navigation.navigate('Content')}
+                        color= '#333'
+                    />
+                </View>
+                    <View style={styles.submitLogin}>
+                    <Button
+                        title="Sign Up"
+                        onPress={() =>
+                            navigation.navigate('Login')
+                        }
+                        color= '#b7b5b5'
+                    />
+                    </View>
+                </View>
+            </View>
+      </SafeAreaView>
+     )
+}
+const styles = StyleSheet.create({
+    title: {
+        height: 40,
+        margin: 12,
+        color: '#333',
+        fontSize: 30,
+        bottom: 30,
+    },
+    login: {
+        marginTop: '50%',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 0.5,
+        padding: 10,
+        borderRadius: 999999,
+    },
+      submit: {
+        marginLeft: 10,
+        marginRight: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+      submitLogin: {
+        width: 150,
+        padding: 10,
+        marginTop: '20%',
+    },
+      information:{
+        marginLeft: 20,
+    }
+})
+export default Login
