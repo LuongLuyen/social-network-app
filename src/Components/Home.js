302c6de5a742eec6f28f715726bf073375c1dc74
LuongLuyen <luongvanluyen2003@gmail.com>
2023-08-01 08:08:48 +0700

    Screen Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,23 +1,190 @@
-import { StyleSheet,View , Text, Image} from 'react-native'
+import { StyleSheet,View , Text, TextInput, Image, ScrollView} from 'react-native'
+import { useState } from 'react'
+import { SimpleLineIcons } from '@expo/vector-icons'
+import { Ionicons } from '@expo/vector-icons'
+import { FontAwesome5 } from '@expo/vector-icons'
 import Header from "../Components/Header"
+import Avatar from '../Utils/Img/1.png'
+import data from "../Utils/Js/data"
 function Home() {
+    const [name, setName] = useState('')
     return ( 
         <View>
             <Header/>
-            <View style={styles.container}>
-                <Text>
-                    Home
-                </Text>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <View style={styles.you}>
+                        <Image style={styles.youAvatar} source={Avatar}/>
+                        <TextInput
+                            style={styles.input}
+                            onChangeText={setName}
+                            placeholder="Bạn đang nghĩ gì ?"
+                            value={name}
+                        />
+                        <Ionicons style={styles.youUpload} name="cloud-upload-outline" size={24} color="black" />
+                    </View> 
+                    {data.map((item)=>(
+                        <View style={styles.item}
+                            key ={item.id}>
+                                <View style= {styles.itemTitle}>
+                                    <Image style={styles.avt} source={item.avt}/>
+                                    <Text style={styles.title}> {item.name} </Text>
+                                </View>
+                            <Text style={styles.vach}></Text>
+                                <View>
+                                    <Text style={styles.contentText}>{item.content}</Text>
+                                </View>
+                                <View style={styles.content} >
+                                    <Image style={styles.contentImg} source={item.anh}/>
+                                </View>
+                            <Text style={styles.vach}></Text>
+                                <View style={styles.action}>
+                                    <Text style={styles.quantity}>100 Like</Text>
+                                    <Text style={styles.quantity}>300 bình luận</Text>
+                                </View>
+                            <Text style={styles.vach}></Text>
+                                <View style={styles.action}>
+                                    <Text style={styles.feeling}>
+                                        <SimpleLineIcons name="like" size={24} color="black" />
+                                        Like
+                                    </Text>
+                                    <Text style={styles.feeling}>
+                                        <FontAwesome5 name="comment" size={24} color="black" />
+                                        Bình luận
+                                    </Text>
+                                    <Text style={styles.feeling}>
+                                        <Ionicons name="share-outline" size={24} color="black" />
+                                        Chia sẻ
+                                    </Text>
+                                </View> 
+                            <Text style={styles.vach}></Text>
+                            <View style={styles.comment} >
+                                <Image style={styles.commentAvatar} source={item.avt}/>
+                                <View style={styles.userComment}>
+                                    <Text>User</Text>
+                                    <Text>test  binh luan</Text>
+                                </View>
+                            </View>
+                        </View>
+                    ))} 
+                </View>
+            </ScrollView>
         </View>
-     )
+    )
 }
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-        margin: 10
-    }
+        marginLeft: 10,
+        marginRight: 10,
+        borderRadius: 10
+    },
+    you: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        width: '100%',
+        padding: 20,
+        borderRadius:10    
+    },
+    name: {
+        fontSize: 20,
+        color: '#12a4ff'
+    },
+    youAvatar: {
+        marginRight: 10,
+        width: 40,
+        height: 40,
+    },
+    input: {
+        width: '70%',
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderWidth: 0.4,
+        borderRadius: 9999999
+    },
+    youUpload: {
+        paddingTop: 7,
+        width: 40,
+        height: 40,
+        marginLeft: '5%',
+    },
+    item: {
+        marginTop: 20,
+        backgroundColor: '#fff',
+        width: '100%',
+        borderRadius: 10,
+    },
+    itemTitle: {
+        flexDirection: 'row',
+        padding: 10,
+    },
+    container: {
+        margin: 10,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    title: {
+        marginBottom: 8,
+    },
+    category: {
+        width: '100%',
+        height:200
+    },
+    avt: {
+        width: 40,
+        height: 40,
+    },
+    action: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin:10,
+    },
+    contentImg: {
+        width: '100%',
+        height: 180
+    },
+    feeling: {
+       paddingRight: 30,
+       paddingLeft: 30,
+       paddingTop:10,
+       paddingBottom: 10,
+    },
+    quantity: {
+        paddingRight: 100,
+        paddingLeft: 100,
+        paddingTop:10,
+        paddingBottom: 10,
+    },
+    vach: {
+        height: 0.5,
+        backgroundColor: '#c7c3c3'
+    },
+    contentText: {
+        margin: 10,
+        marginBottom: 20,
+    },
+    comment: {
+        flexDirection: 'row',
+        margin: 10,
+    },
+    commentAvatar: {
+        width: 40,
+        height: 40,
+    },
+    userComment: {
+        paddingLeft: 10,
+        paddingRight: 10,
+        backgroundColor: '#c7c3c3',
+        borderRadius: 10,
+    },
 })
 export default Home
---
+++ src/Utils/Js/data.js
@@ -0,0 +1,56 @@
+import Card1 from '../Img/1.png'
+import Card2 from '../Img/2.png'
+import Card3 from '../Img/3.png'
+import Card4 from '../Img/4.png'
+import Card5 from '../Img/5.png'
+import anh1 from '../Img/a.jpg'
+import anh2 from '../Img/b.jpg'
+import anh3 from '../Img/c.jpg'
+import anh4 from '../Img/d.jpg'
+import anh5 from '../Img/e.jpg'
+import anh6 from '../Img/f.jpg'
+
+export default data =[
+    {
+        id:1, 
+        name: 'Người dùng 1', 
+        anh: anh1, 
+        avt: Card1, 
+        content:'Components in React Native are styled using JavaScript. Color properties usually match how CSS works on the web. General guides on the color usage on each platform could be found below:'
+    },
+    {
+        id:2, 
+        name: 'Người dùng 2', 
+        anh: anh2, 
+        avt: Card2, 
+        content: 'PlatformColor lets you reference the platforms color system.'
+    },
+    {
+        id:3, 
+        name: 'Người dùng 3', 
+        anh:anh3, 
+        avt: Card3, 
+        content: 'space-between Evenly space off children across the containers main axis, distributing the remaining space between the children.'
+    },
+    {
+        id:4, 
+        name: 'Người dùng 4', 
+        anh:anh4, 
+        avt: Card4, 
+        content: 'space-around Evenly space off children across the containers main axis, distributing the remaining space around the children. Compared to space-between, using space-around will result in space being distributed to the beginning of the first child and end of the last child.'
+    },
+    {
+        id:5, 
+        name: 'Người dùng 5', 
+        anh:anh5, 
+        avt: Card5, 
+        content:'pace-evenly Evenly distribute children within the alignment container along the main axis'
+    },
+    {
+        id:6, 
+        name: 'Người dùng 6', 
+        anh:anh6, 
+        avt: Card1, 
+        content: 'alignItems describes how to align children along the cross axis of their container'
+    },
+]
