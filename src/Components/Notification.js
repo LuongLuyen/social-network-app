bf515e0349d32a28ca1f67bb43e7bcb0f688a8a2
LuongLuyen <luongvanluyen2003@gmail.com>
2023-08-03 15:58:09 +0700

    Screen Notification

--- src/Components/Notification.js
+++ src/Components/Notification.js
@@ -1,22 +1,84 @@
-import { StyleSheet,View , Text, Image} from 'react-native'
+import { StyleSheet,View , Text, Image, ScrollView} from 'react-native'
+import mes from '../Utils/Img/1.png'
+import chatData from '../Utils/Js/chat'
 import Header from "../Components/Header"
 function Notification() {
     return ( 
         <View>
             <Header/>
-            <View style={styles.container}>
-                <Text>
-                    Notification
-                </Text>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <Text style={styles.status}>Mới</Text>
+                    <View >
+                        <View style={styles.chat}>
+                            <Image style={styles.avatar} source={mes}/>
+                            <View style={styles.chatContent}>
+                                <Text style={styles.userName}>nguoi dung</Text>
+                                <Text> hung da binh luan bai viet cua ban</Text>
+                            </View>
+                        </View>
+                        <Text style={styles.vach}></Text>
+                    </View>
+                    <View >
+                        <View style={styles.chat}>
+                            <Image style={styles.avatar} source={mes}/>
+                            <View style={styles.chatContent}>
+                                <Text style={styles.userName}>nguoi dung</Text>
+                                <Text> hung da binh luan bai viet cua ban</Text>
+                            </View>
+                        </View>
+                        <Text style={styles.vach}></Text>
+                    </View>
+                    <Text style={styles.status}>Trước đó</Text>
+                    {chatData.map((item)=>(
+                        <View key ={item.id}>
+                            <View style={styles.chat}>
+                                <Image style={styles.avatar} source={item.avtar}/>
+                                <View style={styles.chatContent}>
+                                    <Text style={styles.userName}>nguoi dung</Text>
+                                    <Text>{item.content}</Text>
+                                </View>
+                            </View>
+                            <Text style={styles.vach}></Text>
+                        </View>
+                    ))}
+                </View>
+            </ScrollView>
         </View>
      )
 }
 const styles = StyleSheet.create({
     container: {
+    },
+    chat: {
+        flexDirection: 'row',
+        padding: 20,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    avatar: {
+        width: 50,
+        height: 50,
+    },
+    vach: {
+        height: 0.5,
+        backgroundColor: '#c7c3c3',
+        marginLeft: 10,
+        marginRight: 10,
+    },
+    userName: {
+        // fontWeight: 900,
+        marginBottom: 5,
+    },
+    chatContent: {
+        marginLeft: 10
+    },
+    status: {
+        marginLeft: 10,
+        fontSize: 20,
+        // fontWeight: 700,
+        padding: 10,
     }
 })
 export default Notification
---
+++ src/Utils/Js/chat.js
@@ -0,0 +1,15 @@
+import Card1 from '../Img/1.png'
+import Card2 from '../Img/2.png'
+import Card3 from '../Img/3.png'
+import Card4 from '../Img/4.png'
+import Card5 from '../Img/5.png'
+export default data =[
+    {id:1, name: 'Người dùng 1',content:'Components in React Native ', avtar: Card1},
+    {id:2, name: 'Người dùng 2',content: 'PlatformColor lets you reference ',avtar: Card2},
+    {id:3, name: 'Người dùng 3',content: 'space-between Evenly space ',avtar: Card3},
+    {id:4, name: 'Người dùng 5',content:'pace-evenly Evenly distribute ',avtar: Card4},
+    {id:5, name: 'Người dùng 6',content: 'alignItems describes ',avtar: Card5},
+    {id:6, name: 'Người dùng 6',content: 'alignItems describes ',avtar: Card5},
+    {id:7, name: 'Người dùng 6',content: 'alignItems describes ',avtar: Card5},
+    {id:8, name: 'Người dùng 6',content: 'alignItems describes ',avtar: Card5},
+]
